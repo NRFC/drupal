e8e978035a581982c9102955c0aa28a4359c4bbf
Toby Batch <tobias@neontribe.co.uk>
2024-08-16 18:57:51 +0100

    feat: adds tooling for git hub hooks (#1)
    * feat: installed tools
    
    * feat: adds tooling for git hub hooks

---
+++ web/modules/nrfc/nrfc_quick_start/src/tests/NrfcQuickStartCommandsTest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace tests;
+
+use Drupal\Tests\UnitTestCase;
+
+// UnitTestCase
+// KernelTestBase
+// BrowserTestBase
+//  WebDriverTestBase
+class NrfcQuickStartCommandsTest extends UnitTestCase
+{
+  function test(): void
+  {
+    assert(1 == 1);
+  }
+}
